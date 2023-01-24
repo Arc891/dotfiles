c58b78176b025489fe2cc607bdefde8572c90b10
Arc891 <ezra.hulsman@gmail.com>
2023-01-24 14:15:54 +0100

    Made module battery & charging check (mostly) hardware independant

--- scripts/echo_cons.sh
+++ scripts/echo_cons.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 MODE=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
-PERCENT=$(cat /sys/class/power_supply/BAT1/capacity)
-CHARGING=$(cat /sys/class/power_supply/ACAD/online)
+PERCENT=$(cat /sys/class/power_supply/BAT*/capacity)
+CHARGING=$(cat /sys/class/power_supply/A*/online)
 ICON=$(/bin/python ~/.dotfiles/scripts/battery_icon.py $PERCENT $CHARGING)
 
 if [[ $MODE -eq 1 ]]
