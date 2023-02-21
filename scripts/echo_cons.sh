86cdf988530250163b51c0f2aded5460f427cb80
Arc891 <ezra.hulsman@gmail.com>
2023-02-21 14:02:23 +0100

    Added --icon-only flag for Arch polybar

--- scripts/echo_cons.sh
+++ scripts/echo_cons.sh
@@ -1,17 +1,22 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 MODE=$(cat /sys/bus/platform/drivers/ideapad_acpi/VPC2004:00/conservation_mode)
 PERCENT=$(cat /sys/class/power_supply/BAT*/capacity)
 CHARGING=$(cat /sys/class/power_supply/A*/online)
 ICON=$(/bin/python ~/.dotfiles/scripts/battery_icon.py $PERCENT $CHARGING)
 
-if [[ $MODE -eq 1 ]]
-then
-	echo $ICON" 󰌪"
-
+if [ "$1" == "--icon-only" ]; then
+    if [[ $MODE -eq 1 ]]; then
+	    echo "󰌪c"
+    else
+        echo "󱐋f"
+    fi
 else
-	echo $ICON
+    if [[ $MODE -eq 1 ]]; then
+	    echo $ICON" 󰌪"
+    else
+	    echo $ICON
+    fi
 fi
-
 # 󱐋
 
