c893a67ad81ddbabb34e88479bc521588838379d
Arc891 <ezra.hulsman@gmail.com>
2024-02-19 09:25:43 +0100

    Added swww startup to change_bg.sh

--- scripts/change_bg.sh
+++ scripts/change_bg.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 bg=$(find ~/Pictures/FavoriteNordics | shuf -n 1)
 
+swww query || swww init
+
 swww img $bg --transition-fps 60 --transition-type wipe --transition-duration 2
 
 #ln -sf $bg /usr/share/sddm/themes/sdt/wallpaper.jpg
